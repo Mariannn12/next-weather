aa0e1cd22efaeb12981d2fafa65a1c13fc679492
Mariannn12 <duca_mariano@yahoo.it>
2023-07-23 00:01:05 +0200

    next js config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
+  webpack : (config) => {
+    config.experiments = { ...config.experiments, topLevelAwait : true};
+
+    return config;
+  },
   reactStrictMode: false,
+
 }
 
 module.exports = nextConfig

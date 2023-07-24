164023f45266edfea1a9a1e500cdca17af82006d
Mariannn12 <duca_mariano@yahoo.it>
2023-07-24 23:10:27 +0200

    changed secret name

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,7 +8,7 @@ export const authOptions = {
             clientSecret : process.env.GOOGLE_CLIENT_SECRET
         })
     ],
-    secret : process.env.NEXTAUTH_SECRET
+    secret : process.env.NEXT_PUBLIC_SECRET
 }
 
 export default NextAuth({
@@ -18,5 +18,5 @@ export default NextAuth({
             clientSecret : process.env.GOOGLE_CLIENT_SECRET
         })
     ],
-    secret : process.env.NEXTAUTH_SECRET
+    secret : process.env.NEXT_PUBLIC_SECRET
 })
